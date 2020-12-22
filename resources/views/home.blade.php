a6ff3aabbfa12fdde9ed52328edbed178b5baf5f
Kyle Mercado <iseeeher@gmail.com>
2020-12-22 08:12:10 +0800

    Added TodoList controller and UI template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('template')
+
+@section('content')
+    <div class="container">
+        <h1>TODO App</h1>
+
+        <div class="jumbotron">
+            <button class="btn btn-primary">Add</button>
+            <button class="btn btn-primary">Edit</button>
+            <button class="btn btn-primary">Delete</button>
+        </div>
+        
+    </div>
+@endsection
